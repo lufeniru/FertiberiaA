afd6b5972de6def1d4f8bf9a9c2033c1ffbc8920
Jaime <jaime.mansilla98@gmail.com>
2019-01-30 10:20:35 +0100

    insertar 2.0

--- app/Http/Controllers/controladorJoaquin.php
+++ app/Http/Controllers/controladorJoaquin.php
@@ -5,47 +5,53 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class controladorJoaquin extends Controller
-{
+class controladorJoaquin extends Controller {
+
     function introducirDatos(Request $req) {
-        /**** las sesiones para coger los campos e insertar **/
+        /*         * ** las sesiones para coger los campos e insertar * */
         $elementos = \Session::get('elementos');
         $compuesto = \Session::get('compuesto');
-        dd ($compuesto[0]->compuesto, $elementos);
-        $fecha_hora=$req->get('fechahora');
-        $planta=\DB::select("select compuestos.planta from compuestos where compuestos.compuesto='".$compuesto[0]->compuesto."'");
-        $valores=($req->get('valor'));
-        $tanque=($req->get('tanque'));
-        
-        $i=0;
+        $fecha_hora = $req->get('fechahora');
+        $planta = \DB::select("select compuestos.planta from compuestos where compuestos.compuesto='" . $compuesto[0]->compuesto . "'");
+        $valores = ($req->get('valor'));
+        $tanque = ($req->get('tanque'));
+
+        $i = 0;
         foreach ($valores as $valor) {
             \DB::table('tabla_tocha')->insert([
                 'fechahora' => $fecha_hora,
-                'planta' => $planta,
+                'planta' => $planta[0]->planta,
                 'tanque' => $tanque,
-                'id_compuesto' => $compuesto[0]->compuesto,
+                'id_compuesto' => $compuesto[0]->id_compuesto,
                 'id_elemento' => $elementos[$i]->id_elem,
-                'condicion' => $elementos->condicion,
-                'valor' => $elementos->valor,
-                'simbolo' => $elementos->simbolo,
+                'condicion' => $elementos[$i]->condicion,
+                'valor' => $elementos[$i]->valor,
+                'simbolo' => $elementos[$i]->simbolo,
                 'lectura' => $valor
             ]);
             $i++;
         }
-        
-        
-        
         //si hay granulometria
-        if ($req->get('granulometria')!=null) {
-            $granus=$req->get('granulometria');
+        if ($req->get('granulometria') != null) {
+            $granus = $req->get('granulometria');
+            $gran = \Session::get('granu');
+            $i = 0;
+            foreach ($granus as $g) {
+                \DB::table('tabla_tocha_granu')->insert([
+                    'fechahora' => $fecha_hora,
+                    'planta' => $planta[0]->planta,
+                    'tanque' => $tanque,
+                    'id_compuesto' => $compuesto[0]->id_compuesto,
+                    'valor_granu' => $gran[$i]->valor,
+                    'condicion' => $gran[$i]->condicion,
+                    'valor1' => $gran[$i]->valor1,
+                    'simbolo' => $gran[$i]->simbolo,
+                    'lectura' => $g
+                ]);
+                $i++;
+            }
         }
-        if ($req->get('granulometria')!=null) {
-            dd('si');
-        }else{
-            dd('no');
-        }
-        
+        echo '<script>alert("Insertado con exito");</script>';
     }
-    
-    
+
 }
