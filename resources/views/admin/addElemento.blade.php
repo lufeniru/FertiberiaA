340bc22492229268a2b434516a7f8c23bf602165
Jaime <jaime.mansilla98@gmail.com>
2019-02-07 10:02:16 +0100

    actualizacion dia 7

--- resources/views/admin/addElemento.blade.php
+++ resources/views/admin/addElemento.blade.php
@@ -8,34 +8,106 @@ Añadir Elemento
 <div class="container" style="text-align: center">
     <form action="addElemento" name="addElemento" method="post">
         {{ csrf_field() }}
-        <h3>Advertencia: se está modificando la estructura de la aplicación, podría causar fallos inesperados, no contemplados en el desarrollo de la aplicación</h3><br>
-        Compuesto al que pertenece: 
-        Planta: <select name="planta" id="planta">
-            <?php
-            foreach ($plantas as $p) {
-                echo '<option value="' . $p->id_planta . '">' . $p->nombre . '</option>';
-            }
-            ?>
-        </select>
+        <div class="row">
+            <div class="col-12"><h3>Advertencia: se está modificando la estructura de la aplicación, podría causar fallos inesperados, no contemplados en el desarrollo de la aplicación</h3></div>
 
-        Nombre completo del elemento: <input type="text" name="nombreElemento">
-        Identificador del elemento (ej: Nitrato Total -> NiTot): 
-        <input type="text" name="idElem"><br>
-        Condición:
-        <select name="condicion">
-            <option selected="" value="null">Sin condición</option>
-            <option value=">"> Mayor que '>'</option>
-            <option value="<"> Menor que '<'</option>
-            <option value="<>"> Entre '< >'</option>
-        </select><br>
-        Valor/es de la condición:
-        <input type="number" name="valor1" value="0">
-        <input type="number" name="valor2" value="0"><br>
-        ¿En qué unidad se mide este elemento?
-        <input type="text" name="simbolo" value=""><br>
-        <input type="submit" name="btAddElemento" value="Aceptar">
-    </form>
+            <div class="col-6"><h4>Planta:</h4> <select name="planta" class="custom-select" id="planta">
+                    <?php
+                    foreach ($plantas as $p) {
+                        echo '<option value="' . $p->id_planta . '">' . $p->nombre . '</option>';
+                    }
+                    ?>
+                </select></div>
+            <div id="comp" class="col-6">
+                <h4>Compuestos:</h4>
+                <select class="custom-select">
+
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6"><h4>Nombre completo del elemento:</h4> <input type="text" class="form-control" name="nombreElemento"></div>
+            <div class="col-6"> <h4>Identificador del elemento:</h4> <input type="text" class="form-control" name="idElem" placeholder="(ej: Nitrato Total -> NiTot)"></div>
+            <div class="col-12"> 
+                <div class="row">
+                    <div class="col-6"> <h4>Condición:</h4>
+                        <select name="condicion" id="condicion" class="custom-select">
+                            <option selected value="null">Sin condición</option>
+                            <option value=">"> Mayor que '>'</option>
+                            <option value="<"> Menor que '<'</option>
+                            <option value="<>"> Entre '< >'</option>
+                        </select>
+                    </div>
+                    <div class="col-6">
+                        <h4>Valor/es de la condición:</h4>
+                        <input type="number" class="form-control" name="valor1" id="val1" disabled placeholder="Valor 1">
+                        <input type="number" hidden class="form-control" id="val2" name="valor2" placeholder="Valor 2">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <h4>¿En qué unidad se mide este elemento?</h4>
+                <input type="text" class="form-control" name="simbolo">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <input type="submit" class="btn btn-info" name="btAddElemento" value="Aceptar">
+            </div>
+        </div>
+
 </div>
+</form>
+</div>
+
+<script>
+    $.ajaxSetup({
+    headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+    });
+    $('#planta').change(function () {
+    //we will send data and recive data fom our AjaxController
+    $.ajax({
+    url: 'sacarcomp',
+            data: {'planta': $("#planta").val()},
+            type: 'post',
+            success: function (response) {
+            $("#comp").html(response);
+            },
+            statusCode: {
+            404: function () {
+            alert('web not found');
+            }
+            },
+            error: function (x, xs, xt) {
+            //nos dara el error si es que hay alguno
+            window.open(JSON.stringify(x));
+            alert('error: ' + JSON.stringify(x) + "\n error string: " + xs + "\n error throwed: " + xt);
+            }
+    });
+    });
+    $("#condicion").change(function () {
+    if ($("#condicion").val() !== 'null'){
+    if ($("#condicion").val() === '<>') {
+    $("#val2").removeAttr('hidden');
+    $("#val1").removeAttr('disabled');
+    } else {
+    $("#val2").attr('hidden', 'true');
+    $("#val1").removeAttr('disabled');
+    }
+    } else{
+    $("#val2").attr('hidden', 'true');
+    $("#val1").attr('disabled', 'true');
+    }
+    });
+    
+
+
+
+</script>
 
 @endsection
 
--- resources/views/laboratorio/Laboratorio.blade.php
+++ resources/views/laboratorio/Laboratorio.blade.php
@@ -5,8 +5,16 @@ Laboratorio
 @endsection
 
 @section('cuerpo')
-   
-
+<div class="container">
+    <div class="row">
+        <nav aria-label="breadcrumb" class="col-12">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index">Inicio</a></li>
+                <li class="breadcrumb-item active"aria-current="page">Laboratorio</li>
+            </ol>
+        </nav>
+    </div>
+</div>
 @endsection
 
 
