a42ee5c1b064644601cc488e9db68b8e7a9bb460
Joaquin <joaquinrubiobmx@gmail.com>
2019-02-14 09:32:51 +0100

    Migas de pan admin

--- resources/views/admin/addElemento.blade.php
+++ resources/views/admin/addElemento.blade.php
@@ -5,6 +5,17 @@ Añadir Elemento
 @endsection
 
 @section('cuerpo')
+<div class="container">
+    <div class="row">
+        <nav aria-label="breadcrumb" class="col-12">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index">Inicio</a></li>
+                <li class="breadcrumb-item active"><a href="admin">Administrador</a></li>
+                <li class="breadcrumb-item active"aria-current="page">Añadir elemento</li>
+            </ol>
+        </nav>
+    </div>
+</div>
 
 <div class="container" style="text-align: center">
     <form action="addElemento" name="addElemento" method="post">
