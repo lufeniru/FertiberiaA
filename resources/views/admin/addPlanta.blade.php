969160850b3f9e83be0637832d770db157d14e96
Jaime <jaime.mansilla98@gmail.com>
2019-02-18 12:51:59 +0100

    cambio alerts

--- resources/views/admin/addPlanta.blade.php
+++ resources/views/admin/addPlanta.blade.php
@@ -18,14 +18,25 @@ Añadir Planta
 </div>
 
 <div class="container">
-    <form action="addPlanta" name="addPlanta" method="POST" class="row" style="margin-top: 20px">
+    <form action="addPlanta" name="addPlanta" method="POST" onsubmit="alerta()" class="row" style="margin-top: 20px">
         {{ csrf_field() }}
-        <div class="offset-2 col-3">Nombre de planta: <input type="text" class="form-control" name="nombre" placeholder="ej(UREA)"></div>
+        <div class="offset-2 col-3">Nombre de planta: <input type="text" class="form-control" id="planta" name="nombre" placeholder="ej(UREA)"></div>
         <div class="col-3">Descripcion: <input type="text" name="descripcion" class="form-control" placeholder="ej(Planta Urea)"></div>
         <div class="col-4" style="margin-top: 22px"><input type="submit" class="btn btn-info" name="btAddPlanta" value="Añadir"></div>
     </form>
 </div>
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script>
+        function alerta() {
+            Swal.fire({
+                position: 'top-end',
+                type: 'success',
+                title: "Planta " + $("#planta").val() + " añadida con exito",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+</script>
 @endsection
 
 
