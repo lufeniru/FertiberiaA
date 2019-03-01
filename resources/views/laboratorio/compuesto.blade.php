c53cf1729d7bc4ce499fe0dd1a5bc7727ae8a62f
Jaime <jaime.mansilla98@gmail.com>
2019-03-01 13:13:52 +0100

    comprobación compuestos

--- resources/views/laboratorio/compuesto.blade.php
+++ resources/views/laboratorio/compuesto.blade.php
@@ -5,6 +5,20 @@ Laboratorio
 @endsection
 
 @section('cuerpo')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script>
+        function nohay(){
+            Swal.fire({
+                position: 'center',
+                type: 'error',
+                title: "No Existen compuestos para esta planta",
+                showConfirmButton: false,
+                footer: '<a href="javascript:window.history.back();">Volver</a>'
+            });
+        }
+       
+        
+</script>
 <?php $existe = \Session::get('planta'); ?>
     <!--parte realizada por Jaime-->
     <div class="row">
@@ -22,13 +36,22 @@ Laboratorio
         <form action="elementos" method="post" class="col-12">
             {{ csrf_field() }}
             <?php
+           
+                
+            
             $compuestos = \Session::get('compuestos');
+             if (count($compuestos)!=0) {
             echo '<select name="compuesto" class="custom-select offset-2 col-lg-7">';
             foreach ($compuestos as $comp) {
                 echo '<option value="' . $comp->id_compuesto . '">' . $comp->compuesto . '</option>';
             }
             ?>
             <input type="submit" class="btn btn-info offset-2 col-2" name="boton" value="Mostrar">
+                <?php 
+                }else{
+               echo '<script>nohay();</script>';
+           }
+                ?>
         </form>
     </div>
 @endsection
